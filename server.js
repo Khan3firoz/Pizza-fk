9bec26d558708790eb6711dba85eaf26c16cee51
Firoz <khan3firoz@gmail.com>
2020-12-28 16:07:05 +0530

    connect to live db

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3300
+
 const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
